644f8db651b6c3201d7824c637b0d1ec81a7486d
Tomer Klein <tomer.klein@gmail.com>
2022-01-26 18:27:25 +0200

    Fixing fastAPI swagger documrntation

--- app/app.py
+++ app/app.py
@@ -58,7 +58,7 @@ tags_metadata = [
 ]
 
 
-app = FastAPI(title="Apprise API", description="Send multi channel notification using single endpoint", version=GetVersionFromFle(), contact={"name":"Tomer Klein","email":"tomer.klein@gmail.com","url":"https://github.com/t0mer/apprise-api-bridge"})
+app = FastAPI(title="Apprise API", description="Send multi channel notification using single endpoint", version=GetVersionFromFle(), openapi_tags=tags_metadata,contact={"name":"Tomer Klein","email":"tomer.klein@gmail.com","url":"https://github.com/t0mer/apprise-api-bridge"})
 logger.info("Configuring app")
 app.mount("/dist", StaticFiles(directory="dist"), name="dist")
 app.mount("/js", StaticFiles(directory="dist/js"), name="js")
